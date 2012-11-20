e01db8450d313cc18d036dce0153255967336e80
Anton Kolesov <akolesov@synopsys.com>
2012-11-20 17:51:58 +0400

    Edit delete script so it will only change permissions.

--- linux/delete.sh
+++ linux/delete.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-for i in ${WORKSPACE}/*
-do
-	echo remove $i
-	chown -R akolesov $i
-	chmod -R 777 $i
-done
-
-for i in /remote/arctools/akolesov/jenkins/linux_3.2_default/*
-do
-    echo remove $i
-    chown -R akolesov $i
-    chmod -R 777 $i
-done
+echo Changing permissions for build directory.
+chmod -R 777 ${WORKSPACE}/${BUILD_NUMBER}/
 
